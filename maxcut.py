9d474225b56effe8fbbaf63342620559d33ee6a9
Will <me@willpowell.uk>
2023-11-21 16:36:49 +0000

    split into fns

--- maxcut.py
+++ maxcut.py
@@ -5,6 +5,7 @@ import networkx as nx
 import random
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+import time
 
 
 def generate_edges(num_of_nodes, num_of_edges):
@@ -23,54 +24,79 @@ def generate_edges(num_of_nodes, num_of_edges):
     return list(edges)
 
 
-num_of_nodes = 8
-num_of_edges = 16
+def Goemans_Williamson_Max_Cut(edges):
+    # Create a symmetric matrix variable
+    X = cp.Variable((num_of_nodes, num_of_nodes), symmetric=True)
 
-# Generate edges for a max cut problem
-edges = generate_edges(num_of_nodes, num_of_edges)
-print(edges)
+    # Constraints
+    constraints = [X >> 0]  # X is positive semidefinite
+    constraints += [X[i, i] == 1 for i in range(num_of_nodes)]  # Diagonals must be 1
 
-# Create a symmetric matrix variable
-X = cp.Variable((num_of_nodes, num_of_nodes), symmetric=True)
+    # Objective function (from paper)
+    objective = cp.Maximize(sum(0.5 * (1 - X[i, j]) for i, j in edges))
 
-# Constraints
-constraints = [X >> 0]  # X is positive semidefinite
-constraints += [X[i, i] == 1 for i in range(num_of_nodes)]  # Diagonals must be 1
+    # Solve problem based on objective and constraints
+    prob = cp.Problem(objective, constraints)
+    prob.solve()
+    X_solution = X.value
 
-# Objective function (from paper)
-objective = cp.Maximize(sum(0.5 * (1 - X[i, j]) for i, j in edges))
+    # Generate a random hyperplane
+    u = np.random.randn(num_of_nodes)
 
-# Solve problem based on objective and constraints
-prob = cp.Problem(objective, constraints)
-prob.solve()
-X_solution = X.value
+    # Project onto the hyperplane and classify
+    x_projected = sp.linalg.sqrtm(X_solution)
+    cut = np.sign(x_projected @ u)
+    cut = cut.real.astype(np.int32)
 
-# Generate a random hyperplane
-u = np.random.randn(num_of_nodes)
+    return cut
 
-# Project onto the hyperplane and classify
-x_projected = sp.linalg.sqrtm(X_solution)
-cut = np.sign(x_projected @ u)
-cut = cut.real.astype(np.int32)
+def plot_max_cut(edges, cut, runtime):
+    # Initialize the graph
+    G = nx.Graph()
+    G.add_edges_from(edges)
 
-# Initialize the graph
-G = nx.Graph()
-G.add_edges_from(edges)
+    # Create a layout for our nodes 
+    layout = nx.spring_layout(G)
 
-# Create a layout for our nodes 
-layout = nx.spring_layout(G)
+    # Initialize the figure
+    fig, ax = plt.subplots(figsize=(8, 5))
 
-# Initialize the figure
-fig, ax = plt.subplots(figsize=(8, 5))
+    # Increase axes limits for margin
+    ax.set_xlim([min(x for x, _ in layout.values()) - 0.1, max(x for x, _ in layout.values()) + 0.1])
+    ax.set_ylim([min(y for _, y in layout.values()) - 0.1, max(y for _, y in layout.values()) + 0.1])
 
-# Draw the graph
-colors = ['red' if c == 1 else 'blue' for c in cut]
-nx.draw(G, pos=layout, node_color=colors, with_labels=True, ax=ax)
+    # Draw the graph
+    colors = ['red' if c == 1 else 'blue' for c in cut]
+    nx.draw(G, pos=layout, node_color=colors, with_labels=True, ax=ax)
 
-# # Drawing a dotted line for the max cut
-# for edge in edges:
-#     if cut[edge[0]] != cut[edge[1]]:
-#         # This edge is part of the cut, draw a dotted line
-#         points = np.array([layout[edge[0]], layout[edge[1]]])
-#         plt.plot(points[:, 0], points[:, 1], linestyle='--', color='blue')
-plt.show()
+    # Drawing a dotted line for the max cut
+    for edge in edges:
+        if cut[edge[0]] != cut[edge[1]]:
+            # This edge is part of the cut, draw a dotted line
+            points = np.array([layout[edge[0]], layout[edge[1]]])
+            plt.plot(points[:, 0], points[:, 1], color='blue')
+
+    # Add runtime to the plot with padding
+    plt.text(0.05, 0.95, f'Runtime: {runtime:.2f}s', transform=ax.transAxes, fontsize=10, verticalalignment='top', bbox=dict(facecolor='white', alpha=0.5, pad=5))
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    # Nodes and edges
+    num_of_nodes = 8
+    num_of_edges = 16
+
+    # Generate edges for a max cut problem
+    edges = generate_edges(num_of_nodes, num_of_edges)
+    print(edges)
+    # start a timer
+    start_time = time.time()
+
+    cut = Goemans_Williamson_Max_Cut(edges)
+    
+    # stop timer and calculate the runtime
+    end_time = time.time()
+    runtime = end_time - start_time
+
+    plot_max_cut(edges, cut, runtime)
